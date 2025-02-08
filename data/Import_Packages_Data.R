b1be28700c7478cee6ce0e218ed6ca9c41c13f95
Jacob Cochran <78105426+jcochran-usfws@users.noreply.github.com>
2025-02-07 19:47:55 -0500

    Updated inputs and developed document
    The file should be in a good spot not to work together on it.

--- data/Import_Packages_Data.R
+++ data/Import_Packages_Data.R
@@ -24,7 +24,19 @@ ipak(requiredPackages)
 
 ### Data and File Import
 
-IP <- read.csv(here("data", "projected_effort.csv"), header=T) %>%  # Reads in the .CSV file from the folder path, change csv to update report
-  mutate(., Priority = factor(Priority, levels = c("High", "Moderate", "Low")))
+IP <- read_excel("../data/projected_effort.xlsx", sheet = 1) %>% 
+  mutate(., Priority = factor(Priority, levels = c("High", "Moderate", "Low"))) %>% 
+  mutate(., Basin = factor(Basin, levels = c("Lake Erie", "Lake Ontario")))
 
-metaB <- read.csv(here("data", "metaB_effort.csv"), header=T)
+metaB <- read_excel("../data/metaB_effort.xlsx", sheet = 1) %>%  
+  mutate(., Priority = factor(Priority, levels = c("High", "Moderate", "Low"))) %>% 
+  mutate(., Basin = factor(Basin, levels = c("Lake Erie", "Lake Ontario")))
+
+Lake_sf <- st_read(here("shapefiles/GL_Outline", "glgis_gl_shore_noaa_70k.shp"))
+cropped <- st_crop(Lake_sf, xmin = -73, xmax = -81,
+                   ymin = 41.5, ymax = 45.4)
+NY_sf <- st_read(here("shapefiles/NYS_Civil_Boundaries.shp/NYS_Civil_Boundaries_SHP",  "State_Shoreline.shp"))
+
+US_sf <- st_read(here("shapefiles/States_shapefile", "States_shapefile.shp")) %>% 
+  filter(State_Code %in% c('PA'))
+#US_cropped <- st_crop(US_sf, xmin = -73, xmax = -81, ymin = 41.5, ymax = 45.4)
